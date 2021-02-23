ea65fa099741e0fd99fdf21fbec51f5ce2b8b682
yv <>
2021-02-23 04:53:22 +0200

    fix typo

--- token/PastaToken.sol
+++ token/PastaToken.sol
@@ -2,8 +2,8 @@ pragma solidity 0.6.12;
 
 import "./token/BEP20/BEP20.sol";
 
-// RamenToken with Governance.
-contract RamenToken is BEP20('Pasta Token', 'Pasta') {
+// PastaToken with Governance.
+contract PastaToken is BEP20('Pasta Token', 'Pasta') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
